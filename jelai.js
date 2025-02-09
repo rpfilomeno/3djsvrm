a46b48ea58e1addc6511c37fc79b106be7587567
Roger Filomeno <rpfilomeno@gmail.com>
2025-02-09 18:32:12 +0800

    initial

---
+++ jelai.js
@@ -0,0 +1,253 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { VRMLoaderPlugin, VRMUtils } from '@pixiv/three-vrm'
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+
+const container = document.getElementById('model-container');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+let currentMixer = undefined;
+let currentVrm = undefined;
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize(container.clientWidth, container.clientHeight);
+container.appendChild(renderer.domElement);
+
+// Add lights
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, Math.PI);
+directionalLight.position.set(1.0, 1.0, 1.0).normalize();
+scene.add(directionalLight);
+
+
+// Load model
+const loader = new GLTFLoader();
+loader.register( ( parser ) => {
+
+    return new VRMLoaderPlugin( parser, { autoUpdateHumanBones: true } );
+
+} );
+
+
+loader.load(
+    // URL of the VRM you want to load
+    'https://cdn.rogverse.fyi/jelai.vrm',
+
+    // called when the resource is loaded
+    (gltf) => {
+        const vrm = gltf.userData.vrm;
+
+        VRMUtils.removeUnnecessaryVertices( gltf.scene );
+        VRMUtils.combineSkeletons( gltf.scene );
+        VRMUtils.combineMorphs( vrm );
+
+        scene.add(vrm.scene);
+        vrm.scene.traverse( ( obj ) => { obj.frustumCulled = false; } );
+
+
+        currentMixer = new THREE.AnimationMixer( vrm.scene );
+        loadMixamoAnimation( "https://cdn.rogverse.fyi/silly.fbx", vrm ).then( ( clip ) => {
+            currentMixer.clipAction( clip ).play();
+            currentMixer.timeScale = 1.0;
+
+        } );
+
+        VRMUtils.rotateVRM0( vrm );
+        currentVrm = vrm
+        
+        console.log(vrm);
+
+
+    },
+
+    // called while loading is progressing
+    (progress) => console.log('Loading model...', 100.0 * (progress.loaded / progress.total), '%'),
+
+    // called when loading has errors
+    (error) => console.error(error),
+);
+
+
+// Camera positioning
+
+camera.position.set( 0.0, 1.0, 1.0 );
+console.log(camera)
+
+
+// Orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.screenSpacePanning = true;
+controls.target.set( 0.0, 1.0, 0.0 );
+controls.update()
+
+// Animation loop
+const clock = new THREE.Clock();
+function animate() {
+    requestAnimationFrame( animate );
+    const deltaTime = clock.getDelta();
+    if ( currentMixer ) {
+        currentMixer.update( deltaTime );
+    }
+    if ( currentVrm ) {
+        currentVrm.update( deltaTime );
+    }
+    renderer.render( scene, camera );
+}
+
+animate();
+
+// Handle window resize
+window.addEventListener('resize', () => {
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.clientWidth, container.clientHeight);
+});
+
+
+function loadMixamoAnimation( url, vrm ) {
+
+	const loader = new FBXLoader(); // A loader which loads FBX
+	return loader.loadAsync( url ).then( ( asset ) => {
+
+		const clip = THREE.AnimationClip.findByName( asset.animations, 'mixamo.com' ); // extract the AnimationClip
+
+		const tracks = []; // KeyframeTracks compatible with VRM will be added here
+
+		const restRotationInverse = new THREE.Quaternion();
+		const parentRestWorldRotation = new THREE.Quaternion();
+		const _quatA = new THREE.Quaternion();
+		const _vec3 = new THREE.Vector3();
+
+		// Adjust with reference to hips height.
+		const motionHipsHeight = asset.getObjectByName( 'mixamorigHips' ).position.y;
+		const vrmHipsY = vrm.humanoid?.getNormalizedBoneNode( 'hips' ).getWorldPosition( _vec3 ).y;
+		const vrmRootY = vrm.scene.getWorldPosition( _vec3 ).y;
+		const vrmHipsHeight = Math.abs( vrmHipsY - vrmRootY );
+		const hipsPositionScale = vrmHipsHeight / motionHipsHeight;
+
+		clip.tracks.forEach( ( track ) => {
+
+			// Convert each tracks for VRM use, and push to `tracks`
+			const trackSplitted = track.name.split( '.' );
+			const mixamoRigName = trackSplitted[ 0 ];
+			const vrmBoneName = mixamoVRMRigMap[ mixamoRigName ];
+			const vrmNodeName = vrm.humanoid?.getNormalizedBoneNode( vrmBoneName )?.name;
+			const mixamoRigNode = asset.getObjectByName( mixamoRigName );
+
+			if ( vrmNodeName != null ) {
+
+				const propertyName = trackSplitted[ 1 ];
+
+				// Store rotations of rest-pose.
+				mixamoRigNode.getWorldQuaternion( restRotationInverse ).invert();
+				mixamoRigNode.parent.getWorldQuaternion( parentRestWorldRotation );
+
+				if ( track instanceof THREE.QuaternionKeyframeTrack ) {
+
+					// Retarget rotation of mixamoRig to NormalizedBone.
+					for ( let i = 0; i < track.values.length; i += 4 ) {
+
+						const flatQuaternion = track.values.slice( i, i + 4 );
+
+						_quatA.fromArray( flatQuaternion );
+
+						// 親のレスト時ワールド回転 * トラックの回転 * レスト時ワールド回転の逆
+						_quatA
+							.premultiply( parentRestWorldRotation )
+							.multiply( restRotationInverse );
+
+						_quatA.toArray( flatQuaternion );
+
+						flatQuaternion.forEach( ( v, index ) => {
+
+							track.values[ index + i ] = v;
+
+						} );
+
+					}
+
+					tracks.push(
+						new THREE.QuaternionKeyframeTrack(
+							`${vrmNodeName}.${propertyName}`,
+							track.times,
+							track.values.map( ( v, i ) => ( vrm.meta?.metaVersion === '0' && i % 2 === 0 ? - v : v ) ),
+						),
+					);
+
+				} else if ( track instanceof THREE.VectorKeyframeTrack ) {
+
+					const value = track.values.map( ( v, i ) => ( vrm.meta?.metaVersion === '0' && i % 3 !== 1 ? - v : v ) * hipsPositionScale );
+					tracks.push( new THREE.VectorKeyframeTrack( `${vrmNodeName}.${propertyName}`, track.times, value ) );
+
+				}
+
+			}
+
+		} );
+
+		return new THREE.AnimationClip( 'vrmAnimation', clip.duration, tracks );
+
+	} );
+
+}
+
+const mixamoVRMRigMap = {
+	mixamorigHips: 'hips',
+	mixamorigSpine: 'spine',
+	mixamorigSpine1: 'chest',
+	mixamorigSpine2: 'upperChest',
+	mixamorigNeck: 'neck',
+	mixamorigHead: 'head',
+	mixamorigLeftShoulder: 'leftShoulder',
+	mixamorigLeftArm: 'leftUpperArm',
+	mixamorigLeftForeArm: 'leftLowerArm',
+	mixamorigLeftHand: 'leftHand',
+	mixamorigLeftHandThumb1: 'leftThumbMetacarpal',
+	mixamorigLeftHandThumb2: 'leftThumbProximal',
+	mixamorigLeftHandThumb3: 'leftThumbDistal',
+	mixamorigLeftHandIndex1: 'leftIndexProximal',
+	mixamorigLeftHandIndex2: 'leftIndexIntermediate',
+	mixamorigLeftHandIndex3: 'leftIndexDistal',
+	mixamorigLeftHandMiddle1: 'leftMiddleProximal',
+	mixamorigLeftHandMiddle2: 'leftMiddleIntermediate',
+	mixamorigLeftHandMiddle3: 'leftMiddleDistal',
+	mixamorigLeftHandRing1: 'leftRingProximal',
+	mixamorigLeftHandRing2: 'leftRingIntermediate',
+	mixamorigLeftHandRing3: 'leftRingDistal',
+	mixamorigLeftHandPinky1: 'leftLittleProximal',
+	mixamorigLeftHandPinky2: 'leftLittleIntermediate',
+	mixamorigLeftHandPinky3: 'leftLittleDistal',
+	mixamorigRightShoulder: 'rightShoulder',
+	mixamorigRightArm: 'rightUpperArm',
+	mixamorigRightForeArm: 'rightLowerArm',
+	mixamorigRightHand: 'rightHand',
+	mixamorigRightHandPinky1: 'rightLittleProximal',
+	mixamorigRightHandPinky2: 'rightLittleIntermediate',
+	mixamorigRightHandPinky3: 'rightLittleDistal',
+	mixamorigRightHandRing1: 'rightRingProximal',
+	mixamorigRightHandRing2: 'rightRingIntermediate',
+	mixamorigRightHandRing3: 'rightRingDistal',
+	mixamorigRightHandMiddle1: 'rightMiddleProximal',
+	mixamorigRightHandMiddle2: 'rightMiddleIntermediate',
+	mixamorigRightHandMiddle3: 'rightMiddleDistal',
+	mixamorigRightHandIndex1: 'rightIndexProximal',
+	mixamorigRightHandIndex2: 'rightIndexIntermediate',
+	mixamorigRightHandIndex3: 'rightIndexDistal',
+	mixamorigRightHandThumb1: 'rightThumbMetacarpal',
+	mixamorigRightHandThumb2: 'rightThumbProximal',
+	mixamorigRightHandThumb3: 'rightThumbDistal',
+	mixamorigLeftUpLeg: 'leftUpperLeg',
+	mixamorigLeftLeg: 'leftLowerLeg',
+	mixamorigLeftFoot: 'leftFoot',
+	mixamorigLeftToeBase: 'leftToes',
+	mixamorigRightUpLeg: 'rightUpperLeg',
+	mixamorigRightLeg: 'rightLowerLeg',
+	mixamorigRightFoot: 'rightFoot',
+	mixamorigRightToeBase: 'rightToes',
+};
